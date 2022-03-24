4c58779733ff2c06eb4eca3b3f12b4f2c0c1cfd3
vinay-debug <vinay.baranwal@reanfoundation.org>
2022-03-24 10:08:47 +0530

    basic user service created

--- src/api/controllers/user.controller.ts
+++ src/api/controllers/user.controller.ts
@@ -3,33 +3,42 @@ import express from 'express';
 import { UserService } from '../../services/user.service';
 import { Loader } from '../../startup/loader';
 import { ResponseHandler } from 'common/response.handler';
+import { ApiError } from 'common/api.error';
 
 export class UserController {
+
     //#region member variables and constructors
+
+    _service: UserService = null;
+
+    constructor() {
+        this._service = Loader.container.resolve(UserService);
+    }
 
     //#endregion
 
     delete = async (request: express.Request, response: express.Response): Promise<void> => {
-        throw new Error('Method not implemented.');
+        throw new ApiError(500, 'Cannot create user!');
     };
 
     getById = async (request: express.Request, response: express.Response): Promise<void> => {
-        throw new Error('Method not implemented.');
+        throw new ApiError(500, 'Cannot create user!');
     };
 
     search = async (request: express.Request, response: express.Response): Promise<void> => {
-        throw new Error('Method not implemented.');
+        throw new ApiError(500, 'Cannot create user!');
     };
 
     create = async (request: express.Request, response: express.Response) => {
         try {
-            const apiResponse = {
-                status: 200,
-                entity: {
-                    name: 'vinay baranwal',
-                    designation: 'software developer'
-                }
-            };
+            // const apiResponse = {
+            //     status: 200,
+            //     entity: {
+            //         name: 'vinay baranwal',
+            //         designation: 'software developer'
+            //     }
+            // };
+            const apiResponse = await this._service.create();
             ResponseHandler.success(request, response, 'User created successfully!', 200, apiResponse )
         }
         catch (err) {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,19 @@
+import { IUserRepo } from 'database/repository.interfaces/user.repo.intrerface';
+import { UserDomainModel } from 'domain.types/user/user.domain.model';
 import { inject, injectable } from 'tsyringe';
 
 @injectable()
-export class UserService {}
+export class UserService {
+
+    create = async ():
+    Promise<any> => {
+        const apiResponse = {
+            status: 200,
+            entity: {
+                name: 'vinay baranwal',
+                designation: 'software developer'
+            }
+        };
+        return apiResponse
+    };
+}
