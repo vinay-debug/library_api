3f33fead9e3df88c588a34b8b7e5755abda2ccc9
vinay-debug <vinay.baranwal@reanfoundation.org>
2022-06-26 08:06:24 +0530

    Fixed authentication while creating user

--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -9,7 +9,7 @@ export const register = (app: express.Application): void => {
     const authenticator = Loader.authenticator;
     const controller = new UserController();
 
-    router.post('/', controller.create);
+    router.post('/',authenticator.authenticateUser, controller.create);
     // router.get('/', authenticator.authenticateUser, controller.search);
     router.get('/:id',authenticator.authenticateUser, controller.getById);
     // router.delete('/:id', authenticator.authenticateUser, controller.delete);
